3a8ee64d8122eab8b3dfebad60b9af949861593b
nsovomaswanganyi <148325838+NsovoWMaswanganyi@users.noreply.github.com>
2026-07-09 13:57:55 +0200

    Created Academic Tracker, Finished with Basic UI

---
+++ academic_tracker_app/src/main/java/com/academic_tracker_app/AcademicTrackerApplication.java
@@ -0,0 +1,19 @@
+package com.academic_tracker_app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AcademicTrackerApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(AcademicTrackerApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ academic_tracker_app/src/main/java/com/academic_tracker_app/Launcher.java
@@ -0,0 +1,9 @@
+package com.academic_tracker_app;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(AcademicTrackerApplication.class, args);
+    }
+}
